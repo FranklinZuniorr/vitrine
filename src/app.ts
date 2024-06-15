252f644cf303a2d15c7c12172e36630fc7bc0f21
Franklin Vieira <franklinbarbs763@gmail.com>
2024-06-15 04:31:15 -0300

    feat(cors): should config cors

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,13 @@ import express, { Request, Response } from 'express';
 import controllers from './Controllers';
 import 'dotenv/config';
 import connectDB from './infra/mongodb/db';
+import cors from 'cors';
 
 connectDB();
 
 const app = express();
 const port = process.env.PORT || 3000;
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
