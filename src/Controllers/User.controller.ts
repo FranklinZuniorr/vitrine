1b1e146c73cbe24934ffd8391c04382c1d0f40bb
Franklin Vieira <franklinbarbs763@gmail.com>
2024-06-17 20:56:54 -0300

    feat(cloudinary): should add middleware to filter base64 to set url in data base

--- src/Controllers/User.controller.ts
+++ src/Controllers/User.controller.ts
@@ -3,13 +3,17 @@ import { UserService } from '../Services/_user/User.service';
 import { UserRepository } from '../Services/_user/repository/User.repository';
 import UserModel from '../Services/_user/repository/models/User.model';
 import { ValidateToken } from '../middlewares/ValidateToken.middleware';
+import { Cloudinary } from '../middlewares/Cloudinary.middleware';
 
 const userController = Router();
 const userRepository: UserRepository = new UserRepository(UserModel);
 const userService: UserService = new UserService(userRepository);
 const validateToken: ValidateToken = new ValidateToken(userRepository);
+const cloudinary: Cloudinary = new Cloudinary();
 
-userController.post('/', (req: Request, res: Response) => userService.newUser(req, res));
+userController.post('/',
+(req: Request, res: Response, next: NextFunction) => cloudinary.filterImage(req, res, next),  
+(req: Request, res: Response) => userService.newUser(req, res));
 userController.post('/login', (req: Request, res: Response) => userService.login(req, res));
 userController.post('/new-token', (req: Request, res: Response) => userService.newToken(req, res));
 userController.post('/new-password/:userEmail', (req: Request, res: Response) => userService.newPassword(req, res));
